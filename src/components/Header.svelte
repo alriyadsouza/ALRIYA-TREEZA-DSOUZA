c0052706e316d486776045872690b4b03ccc7912
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-01 14:34:52 +0530

    Made the navbar cuter

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -10,17 +10,18 @@
   }
 </script>
 
-
-<header class="flex w-full h-16 justify-between items-center p-4 px-8 fixed
-            text-gray-950 dark:text-neutral-50 bg-white/[0.15]
-            dark:bg-black/[0.15] shadow-sm">
-    <a href="/" class=" mr-auto font-bold text-xl" >🍪&nbsp;Alriya</a>
-    <nav>
-      <ul class="list-none flex gap-4">
-        <li><a href="/about">About</a></li>
-        <li><a href="/showcase">Projects</a></li>
-        <li><a href="https://github.com/alriyadsouza">GitHub</a></li>
-        <button on:click={toggleTheme}>Toggle Theme</button>
-      </ul>
-    </nav>
-</header>
+<div class="w-screen flex justify-center">
+  <header class="flex w-[85%] h-16 justify-between items-center py-4 px-6 fixed
+              text-gray-950 dark:text-neutral-50 bg-white/[0.15] m-4
+              shadow-sm border border-white dark:border-black rounded-full">
+      <a href="/" class=" mr-auto font-bold text-xl" >🍪&nbsp;Alriya</a>
+      <nav>
+        <ul class="list-none flex gap-4">
+          <li><a href="/about">About</a></li>
+          <li><a href="/showcase">Projects</a></li>
+          <li><a href="https://github.com/alriyadsouza">GitHub</a></li>
+          <button on:click={toggleTheme}>Toggle Theme</button>
+        </ul>
+      </nav>
+  </header>
+</div>

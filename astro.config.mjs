0d6d884b895a619f3b545e4ed6f83b147a904ba2
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-02 19:41:08 +0530

    Removed Lottie dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from '@astrojs/svelte';
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     svelte(),
-    tailwind()
+    tailwind(),
+    lottie()
   ],
 });

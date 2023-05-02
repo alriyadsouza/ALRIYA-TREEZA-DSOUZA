6d7e76343eda5a211340e260adcc45698154ab22
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-02 20:00:59 +0530

    Fixed dependency error (Lottie)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from '@astrojs/svelte';
-import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     svelte(),
-    tailwind(),
-    lottie()
+    tailwind()
   ],
 });

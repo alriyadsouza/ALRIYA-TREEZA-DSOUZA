9de0ddbfe1a49ca002ccace0f513169597f10012
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-02 23:03:56 +0530

    Made Hero Responsive

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -14,15 +14,16 @@
 </script>
 
 <section class="bg-teal-200 grid w-screen h-screen place-content-center dark:bg-blue-950">
-  <div class="flex">
-    <img src={Rocket} class="w-[300px]" alt="Profile" />
-    <div class="flex flex-col justify-center pr-12 text-blue-950 dark:text-teal-200">
-      <h1 class="text-5xl font-bold" >{props.heading}</h1>
-      <p class="text-xl">{props.text}</p>
-      <div class="flex justify-start items-center gap-6 m-2 mt-6 w-fit icons">
+  <div class="flex flex-col items-center lg:flex-row">
+    <img src={Rocket} class=" w-[200px] md:w-[250px] lg:w-[300px]" alt="Profile" />
+    <div class="flex flex-col justify-center lg:pr-12 text-blue-950 dark:text-teal-200
+                items-center lg:items-start">
+      <h1 class="text-2xl md:text-3xl lg:text-5xl font-bold my-0 md:my-2 lg:my-0" >{props.heading}</h1>
+      <p class="text-sm md:text-lg lg:text-xl">{props.text}</p>
+      <div class="flex justify-start items-center gap-6 m-2 lg:mt-6 w-fit icons">
         {#each icons as icon}
         <a href="{icon.link}" class="link" >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-blue-950 dark:fill-teal-200 w-8">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-blue-950 dark:fill-teal-200 w-6 lg:w-8">
               <path d={icon.path}></path>
             </svg>
           </a>
